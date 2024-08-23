25151c7d387a45a17f0824d0205ba4f3c8fef807
Ulrich-Matthias Schäfer <ulima.ums@googlemail.com>
2024-08-23 18:36:44 +0200

    so many things

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,39 +1,15 @@
-// 1. Import utilities from `astro:content`
-import { z, defineCollection } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
-// 2. Define your collection(s)
-const blogCollection = defineCollection({
-  schema: z.object({
-    draft: z.boolean(),
-    title: z.string(),
-    snippet: z.string(),
-    image: z.object({
-      src: z.string(),
-      alt: z.string(),
+const guide = defineCollection({
+  type: "content",
+  schema: () =>
+    // using zod to define type-safe frontmatter of our mdx files
+    // astro will generate types definitions for our project so we can use them in templates
+    // also it will check every newly created frontmatter in the content/blog directory
+    z.object({
+      title: z.string(),
     }),
-    publishDate: z.string().transform(str => new Date(str)),
-    author: z.string().default('Astroship'),
-    category: z.string(),
-    tags: z.array(z.string()),
-  }),
 });
 
-const teamCollection = defineCollection({
-  schema: z.object({
-    draft: z.boolean(),
-    name: z.string(),
-    title: z.string(),
-    avatar: z.object({
-      src: z.string(),
-      alt: z.string(),
-    }),
-    publishDate: z.string().transform(str => new Date(str)),
-  }),
-});
-
-// 3. Export a single `collections` object to register your collection(s)
-//    This key should match your collection directory name in "src/content"
-export const collections = {
-  'blog': blogCollection,
-  'team': teamCollection,
-};
+// This key should match your collection directory name in "src/content"
+export const collections = { guide };
